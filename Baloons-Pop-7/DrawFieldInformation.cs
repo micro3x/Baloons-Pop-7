bd8a9bbcb8845ab610323c6eabc085b211418d0c
Hristo Turlakov <hr_turlakov@abv.bg>
2014-07-27 22:48:28 +0300

    Implemeted class DrawFieldInformation

--- Baloons-Pop-7/DrawFieldInformation.cs
+++ Baloons-Pop-7/DrawFieldInformation.cs
@@ -1,13 +1,53 @@
 ﻿namespace BalloonsPops
 {
-
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
 
-    class DrawFieldInformation
+    public sealed class DrawFieldInformation
     {
-        //TODO:
+        private static readonly DrawFieldInformation drawer = new DrawFieldInformation();
+
+        public static DrawFieldInformation Instance
+        {
+            get
+            {
+                return drawer;
+            }
+        }
+
+        public void DrawGameField(char[,] tableOfGame)
+        {
+            Console.WriteLine("    0 1 2 3 4 5 6 7 8 9");
+            Console.WriteLine("   ---------------------");
+
+            for (int indexRow = 0; indexRow < tableOfGame.GetLength(0); indexRow++)
+            {
+                Console.Write(indexRow + " | ");
+
+                for (int indexColumn = 0; indexColumn < tableOfGame.GetLength(1); indexColumn++)
+                {
+                    char symbol = tableOfGame[indexRow, indexColumn];
+
+                    if (symbol == '1') Console.BackgroundColor = ConsoleColor.Red;
+                    else if (symbol == '2') Console.BackgroundColor = ConsoleColor.Green;
+                    else if (symbol == '3') Console.BackgroundColor = ConsoleColor.Blue;
+                    else if (symbol == '4') Console.BackgroundColor = ConsoleColor.Yellow;
+                    else Console.ResetColor();
+
+                    Console.Write(symbol + " ");
+                    Console.ResetColor();
+                }
+
+                Console.Write("| ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("   ---------------------");
+        }
+
+        public void DrawWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to “Balloons Pops” game. Please try to pop the balloons." +
+                " Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
+        }
     }
 }
--- Baloons-Pop-7/GameField.cs
+++ Baloons-Pop-7/GameField.cs
@@ -1,13 +1,49 @@
 ﻿namespace BalloonsPops
 {
-
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
 
-    class GameField
+    public class GameField
     {
-        //TODO:
+        private const string SYMBOLS = "1234";
+
+        public GameField(int rows, int cols)
+        {
+            this.Rows = rows;
+            this.Cols = cols;
+            this.TableOfGame = this.InitialiseTableOfGame(this.Rows, this.Cols);
+        }
+
+        public int Rows { get; private set; }
+
+        public int Cols { get; private set; }
+
+        public char[,] TableOfGame { get; private set; }
+
+        private char[,] InitialiseTableOfGame(int rows, int cols)
+        {
+            RandomGenerator rnd = new RandomGenerator();
+            char[,] tableOfGame = new char[rows, cols];
+
+            for (int indexRow = 0; indexRow < rows; indexRow++)
+            {
+                for (int indexColumn = 0; indexColumn < cols; indexColumn++)
+                {
+                    tableOfGame[indexRow, indexColumn] = GetRandomSymbol(rnd);
+                }
+            }
+
+            return tableOfGame;
+        }
+
+        private char GetRandomSymbol(RandomGenerator randomGenerator)
+        {
+            int randomNumber = randomGenerator.GetRandomNumber(0, SYMBOLS.Length);
+            char symbol = SYMBOLS[randomNumber];
+
+            return symbol;
+        }
     }
 }
