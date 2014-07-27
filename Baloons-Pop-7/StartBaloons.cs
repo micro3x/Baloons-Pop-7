74596f894e14eeeb4703fbc47ffe4ace55ee089e
Hristo Turlakov <hr_turlakov@abv.bg>
2014-07-27 22:53:33 +0300

    Implement class StartBaloons

--- Baloons-Pop-7/StartBaloons.cs
+++ Baloons-Pop-7/StartBaloons.cs
@@ -1,16 +1,11 @@
 namespace BalloonsPops
 {
-
     public class StartBaloons
     {
-        //private const byte ROW = 5;
-        //private const byte COLUMN = 10;
         public static void Main()
         {
-            //while()
-            //{
-            GameEngine.Start(); //TODO: load from GameInitialization
-            //}
+            GameEngine playGame = new GameEngine();
+            playGame.Start();
         }
     }
 }
