447f09ff5a3434e50c8100e253916c1ef989a35c
Hristo Turlakov <hr_turlakov@abv.bg>
2014-07-27 22:46:43 +0300

    Implemented class ScoreBoard

--- Baloons-Pop-7/ScoreBoard.cs
+++ Baloons-Pop-7/ScoreBoard.cs
@@ -1,23 +1,41 @@
 ﻿namespace BalloonsPops
 {
-
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
 
     public class ScoreBoard
     {
-        //TODO:
-        private static SortedDictionary<int, string> statistics = new SortedDictionary<int, string>();      
+        private static IDictionary<string, int> statistics = new Dictionary<string, int>();
 
-        public static void PrintAgain()
+        public static void AddPlayer(string name, int games)
+        {
+            if (statistics.ContainsKey(name))
+            {
+                if (games < statistics[name])
+                {
+                    statistics[name] = games;
+                }
+            }
+            else
+            {
+                statistics.Add(name, games);
+            }
+
+            var sortedPlayers = from pair in statistics
+                                orderby pair.Value ascending
+                                select pair;
+
+            statistics = sortedPlayers.ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+
+        public static void Print()
         {
             int points = 0;
 
             Console.WriteLine("Scoreboard:");
 
-            foreach (KeyValuePair<int, string> s in statistics)
+            foreach (KeyValuePair<string, int> s in statistics)
             {
                 if (points == 4)
                 {
@@ -26,7 +44,7 @@
                 else
                 {
                     points++;
-                    Console.WriteLine("{0}. {1} --> {2} moves", points, s.Value, s.Key);
+                    Console.WriteLine("{0}. {1} --> {2} moves", points, s.Key, s.Value);
                 }
             }
         }
